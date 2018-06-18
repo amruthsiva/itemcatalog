c5bd1a5e616d075f8546b014d6c0409299f15c4b
gudivada amrita siva kumar <38848739+amruthsiva@users.noreply.github.com>
2018-06-18 17:40:22 +0530

    Update lotsofcandys.py

--- lotsofcandys.py
+++ lotsofcandys.py
@@ -32,32 +32,29 @@ session.add(candyshop1)
 session.commit()
 
 candy2 = Candy(user_id=1, name="Eclairs", description="Birthday Pack",
-                     price="$200",course = "Appetizer", candyshop=candyshop1)
+               price="$200", course="Appetizer", candyshop=candyshop1)
 
-session.add(candy2 )
+session.add(candy2)
 session.commit()
 
 
 candy1 = Candy(user_id=1, name="Licks", description="Birthday Pack",
-                     price="$248.99", course = "Appetizer",candyshop=candyshop1)
+               price="$248.99", course="Appetizer", candyshop=candyshop1)
 
 session.add(candy1)
 session.commit()
 
 candy2 = Candy(user_id=1, name="Fruitee Fun", description=" Birthday Pack ",
-                     price="$521.50", course = "Appetizer",candyshop=candyshop1)
+               price="$521.50", course="Appetizer", candyshop=candyshop1)
 
 session.add(candy2)
 session.commit()
 
 candy3 = Candy(user_id=1, name="Double Eclairs ", description="Birthday Pack",
-                     price="$356",course = "Appetizer",candyshop=candyshop1)
+               price="$356", course="Appetizer", candyshop=candyshop1)
 
 session.add(candy3)
 session.commit()
-
-
-
 # candies in nestle
 candyshop2 = Candyshop(user_id=1, name="Nestle")
 
@@ -66,53 +63,46 @@ session.commit()
 
 
 candy1 = Candy(user_id=1, name="Kitkat", description="Birthday Pack",
-                     price="$73.99",course = "Appetizer", candyshop=candyshop2)
+               price="$73.99", course="Appetizer", candyshop=candyshop2)
 
-session.add(candy1 )
+session.add(candy1)
 session.commit()
 
-candy2 = Candy(user_id=1, name="Polo",
-                     description="Birthday Pack", price="$257",course = "Appetizer", candyshop=candyshop2)
-
+candy2 = Candy(user_id=1, name="Polo", description="Birthday Pack",
+               price="$257", course="Appetizer", candyshop=candyshop2)
 session.add(candy2)
 session.commit()
 
 candy3 = Candy(user_id=1, name="Milkybar", description="Birthday Pack",
-                     price="$215",course = "Appetizer", candyshop=candyshop2)
+               price="$215", course="Appetizer", candyshop=candyshop2)
 
 session.add(candy3)
 session.commit()
-
-
-
-# candies in Hershey 
-candyshop1 =Candyshop(user_id=1, name="Hershey")
+# candies in Hershey
+candyshop1 = Candyshop(user_id=1, name="Hershey")
 
 session.add(candyshop1)
 session.commit()
 
 
-candy1 = Candy(user_id=1, name="Reese's Peanut Butter Cups", description="Birthday Pack",
-                     price="$84.99",course = "Entree",candyshop=candyshop1)
+candy1 = Candy(user_id=1, name="Reese's Peanut Butter Cups",
+               description="Birthday Pack",
+               price="$84.99", course="Entree", candyshop=candyshop1)
 
 session.add(candy1)
 session.commit()
 
 candy2 = Candy(user_id=1, name="Hershey Bar", description="Birthday Pack",
-                     price="$63.99",course = "Entree",candyshop=candyshop1)
+               price="$63.99", course="Entree", candyshop=candyshop1)
 
 session.add(candy2)
 session.commit()
 
 candy3 = Candy(user_id=1, name="Krackle", description="Birthday Pack",
-                     price="$92.95",course = "Entree",candyshop=candyshop1)
+               price="$92.95", course="Entree", candyshop=candyshop1)
 
 session.add(candy3)
 session.commit()
-
-
-
-
 # candies in mars
 candyshop1 = Candyshop(user_id=1, name="Mars")
 
@@ -121,24 +111,21 @@ session.commit()
 
 
 candy1 = Candy(user_id=1, name="Snickers", description="Birthday Pack",
-                     price="$220.99",course = "Entree",candyshop=candyshop1)
+               price="$220.99", course="Entree", candyshop=candyshop1)
 
 session.add(candy1)
 session.commit()
 
 candy2 = Candy(user_id=1, name="Milkyway", description="Birthday Pack",
-                     price="$225.99",course = "Entree",candyshop=candyshop1)
+               price="$225.99", course="Entree", candyshop=candyshop1)
 
 session.add(candy2)
 session.commit()
 
 candy3 = Candy(user_id=1, name="Twix",
-                     description="Birthday Pack", price="$40.50",course = "Entree",candyshop=candyshop1)
+               description="Birthday Pack", price="$40.50",
+               course="Entree", candyshop=candyshop1)
 
 session.add(candy3)
 session.commit()
-
-
-
-
 print "added all candies!"
