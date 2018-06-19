ba72b4af82296d10fae73c02d494c33fe53e0262
gudivada amrita siva kumar <38848739+amruthsiva@users.noreply.github.com>
2018-06-19 11:46:17 +0530

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
@@ -8,13 +7,13 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class User(Base):
-   __tablename__ = 'user'
 
-   id = Column(Integer, primary_key=True)
-   name = Column(String(250), nullable=False)
-   email= Column(String(250), nullable=False)
-   picture = Column(String(250))
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 
 class Candyshop(Base):
